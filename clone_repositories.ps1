b3270302031c522766388dd7b934660fb326264e
Mateusz.Galasinski <mateusz.galasinski@protonmail.com>
2018-12-28 17:53:46 +0100

    compose, clone repos script

---
+++ clone_repositories.ps1
@@ -0,0 +1,3 @@
+git clone https://github.com/MateuszGalasinski/mysqlDb.git
+git clone https://github.com/MateuszGalasinski/VoteManagerContract.git
+git clone https://github.com/kpilcicki/io-passthrough-api.git
